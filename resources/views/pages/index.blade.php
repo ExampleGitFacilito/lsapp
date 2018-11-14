717f68615e9bdcba92e7002a083df3110273a6bc
Jose Felix Cabrera Venegas <josefelixcv@gmail.com>
2018-11-13 22:01:01 -0500

    Change facebook to red

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,7 +8,7 @@
     </div>
     <div  class="text-center">
         <button type="button" class="btn btn-lg btn-primary">Instagram</button>
-        <button type="button" class="btn btn-lg btn-success">Facebook</button>
+        <button type="button" class="btn btn-lg btn-danger">Facebook</button>
         <button type="button" class="btn btn-lg btn-info">Twitter</button>
         <button type="button" class="btn btn-lg btn-warning">Linked-In</button>
     </div>
