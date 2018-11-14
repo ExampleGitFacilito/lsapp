0ed97750556c8637bc398c94264006b57fcdec14
Jose Felix Cabrera Venegas <josefelixcv@gmail.com>
2018-11-13 21:58:08 -0500

    Adding text to the buttons

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,10 +7,10 @@
         <p><a class="btn btn-lg btn-primary" href="/login" role="button">Login</a> <a class="btn btn-lg btn-success" href="/register" role="button">Register</a></p>
     </div>
     <div  class="text-center">
-        <button type="button" class="btn btn-lg btn-primary">Primary</button>
-        <button type="button" class="btn btn-lg btn-success">Success</button>
-        <button type="button" class="btn btn-lg btn-info">Info</button>
-        <button type="button" class="btn btn-lg btn-warning">Warning</button>
+        <button type="button" class="btn btn-lg btn-primary">Instagram</button>
+        <button type="button" class="btn btn-lg btn-success">Facebook</button>
+        <button type="button" class="btn btn-lg btn-info">Twitter</button>
+        <button type="button" class="btn btn-lg btn-warning">Linked-In</button>
     </div>
 @endsection
 
