0b149e7eb42e0560fd79dd97be3dbd44f5e256fe
Jose Felix Cabrera Venegas <josefelixcv@gmail.com>
2018-11-17 12:16:45 -0500

    Adding second div with buttons

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,6 +11,8 @@
         <button type="button" class="btn btn-lg btn-danger">Facebook</button>
         <button type="button" class="btn btn-lg btn-info">Twitter</button>
         <button type="button" class="btn btn-lg btn-warning">Linked-In</button>
+    </div>
+    <div  class="text-center">
         <button type="button" class="btn btn-lg btn-success">Git Hub</button>
         <button type="button" class="btn btn-lg btn-primary">Snapchat</button>
         <button type="button" class="btn btn-lg btn-info">Reddit</button>
